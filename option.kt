4116b5ed753b1f958976d7aa703e978b2e0228f1
Bashirabdallah <92039124+Bashirabdallah@users.noreply.github.com>
2021-10-06 22:44:28 +0200

    المجموع الأول numbar One
    1/ محمد العاصم مبارك عبدالماجد
    2/بشير عبدالله إبراهيم عبدالمولي
    3/سليمى محمد محمد أحمد
    4/محمد صلاح محمد شريف
    5/نجم الدين على محمد
    6/ابازار عبد اللطيف
    7/إيمان سعيد

---
+++ option.kt
@@ -0,0 +1,18 @@
+class option {
+}
+
+fun main(args: Array<String>) {
+    var x: Int = 3
+    var y  : Int = 17
+    var a: Int = x + y
+    var s: Int = x -y
+    var d: Int = x * y
+    var f =x / y
+    var g =  x % y
+    println(a)
+    println(s)
+    println(d)
+    println(f)
+    println(g)
+
+}
